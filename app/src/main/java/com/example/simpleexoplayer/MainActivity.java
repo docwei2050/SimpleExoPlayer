48dc8c917dd9eb0cc2d7305e432397c53d1d83c6
liweikun <liweikun@codemao.cn>
2020-10-15 19:26:53 +0800

    init commit

---
+++ app/src/main/java/com/example/simpleexoplayer/MainActivity.java
@@ -0,0 +1,315 @@
+package com.example.simpleexoplayer;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.media.MediaDrm;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Pair;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayerLibraryInfo;
+import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.PlaybackPreparer;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.RenderersFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.audio.AudioAttributes;
+import com.google.android.exoplayer2.database.DatabaseProvider;
+import com.google.android.exoplayer2.database.ExoDatabaseProvider;
+import com.google.android.exoplayer2.drm.DefaultDrmSessionManager;
+import com.google.android.exoplayer2.drm.DrmSessionManager;
+import com.google.android.exoplayer2.drm.ExoMediaCrypto;
+import com.google.android.exoplayer2.drm.FrameworkMediaDrm;
+import com.google.android.exoplayer2.drm.HttpMediaDrmCallback;
+import com.google.android.exoplayer2.drm.MediaDrmCallback;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.mediacodec.MediaCodecRenderer;
+import com.google.android.exoplayer2.mediacodec.MediaCodecUtil;
+import com.google.android.exoplayer2.offline.DownloadHelper;
+import com.google.android.exoplayer2.offline.DownloadManager;
+import com.google.android.exoplayer2.offline.DownloadRequest;
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.MergingMediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.source.SingleSampleMediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.RandomTrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.ui.DebugTextViewHelper;
+import com.google.android.exoplayer2.ui.PlayerControlView;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.ui.spherical.SphericalGLSurfaceView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.upstream.FileDataSource;
+import com.google.android.exoplayer2.upstream.HttpDataSource;
+import com.google.android.exoplayer2.upstream.cache.Cache;
+import com.google.android.exoplayer2.upstream.cache.CacheDataSource;
+import com.google.android.exoplayer2.upstream.cache.CacheDataSourceFactory;
+import com.google.android.exoplayer2.upstream.cache.NoOpCacheEvictor;
+import com.google.android.exoplayer2.upstream.cache.SimpleCache;
+import com.google.android.exoplayer2.util.ErrorMessageProvider;
+import com.google.android.exoplayer2.util.EventLogger;
+import com.google.android.exoplayer2.util.Util;
+
+import java.io.File;
+
+/**
+ * Created by liwk on 2020/10/15.
+ */
+public class MainActivity extends AppCompatActivity implements PlayerControlView.VisibilityListener, PlaybackPreparer {
+
+    private PlayerView playerView;
+    private SimpleExoPlayer player;
+    private DefaultTrackSelector.Parameters trackSelectorParameters;
+    private boolean startAutoPlay;
+    private int startWindow;
+    private long startPosition;
+    private MediaSource mediaSource;
+    private DefaultTrackSelector trackSelector;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        playerView = findViewById(R.id.player_view);
+        playerView.setControllerVisibilityListener(this);
+        playerView.setErrorMessageProvider(new PlayerErrorMessageProvider());
+        playerView.requestFocus();
+        //((SphericalGLSurfaceView) playerView.getVideoSurfaceView()).setDefaultStereoMode(C.STEREO_MODE_MONO);
+        DefaultTrackSelector.ParametersBuilder builder =
+                new DefaultTrackSelector.ParametersBuilder(/* context= */ this);
+
+        builder.setTunnelingAudioSessionId(C.generateAudioSessionIdV21(/* context= */ this));
+        trackSelectorParameters = builder.build();
+        clearStartPosition();
+
+    }
+
+    private void clearStartPosition() {
+        startAutoPlay = true;
+        startWindow = C.INDEX_UNSET;
+        startPosition = C.TIME_UNSET;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+            if (playerView != null) {
+                playerView.onResume();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Util.SDK_INT <= 23 || player == null) {
+            initializePlayer();
+            if (playerView != null) {
+                playerView.onResume();
+            }
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            if (playerView != null) {
+                playerView.onPause();
+            }
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            if (playerView != null) {
+                playerView.onPause();
+            }
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+    }
+
+    @Override
+    public void onVisibilityChange(int visibility) {
+
+    }
+
+    private void initializePlayer() {
+        if (player == null) {
+
+            mediaSource = new HlsMediaSource.Factory(buildDataSourceFactory()).createMediaSource(Uri.parse("https://dev-cdn-common.codemao.cn//dev/444/15980142126571-1P1-化妆大派对-数学大冒险.4_p1.m3u8"));
+            TrackSelection.Factory trackSelectionFactory = new AdaptiveTrackSelection.Factory();
+            RenderersFactory renderersFactory =
+                    new DefaultRenderersFactory(this)
+                            .setExtensionRendererMode(DefaultRenderersFactory.EXTENSION_RENDERER_MODE_PREFER);
+
+            trackSelector = new DefaultTrackSelector(/* context= */ this, trackSelectionFactory);
+            trackSelector.setParameters(trackSelectorParameters);
+
+
+            player =
+                    new SimpleExoPlayer.Builder(/* context= */ this, renderersFactory)
+                            .setTrackSelector(trackSelector)
+                            .build();
+            player.addListener(new PlayerEventListener());
+            player.setAudioAttributes(AudioAttributes.DEFAULT, /* handleAudioFocus= */ true);
+            player.setPlayWhenReady(startAutoPlay);
+            player.addAnalyticsListener(new EventLogger(trackSelector));
+            playerView.setPlayer(player);
+            playerView.setPlaybackPreparer(this);
+
+        }
+        player.prepare(mediaSource, false, false);
+
+    }
+
+
+    private void releasePlayer() {
+        if (player != null) {
+            player.release();
+            player = null;
+            mediaSource = null;
+            trackSelector = null;
+        }
+
+    }
+
+    @Override
+    public void preparePlayback() {
+
+    }
+
+
+    private class PlayerErrorMessageProvider implements ErrorMessageProvider<ExoPlaybackException> {
+
+        @Override
+        public Pair<Integer, String> getErrorMessage(ExoPlaybackException e) {
+            String errorString = "播放失败";
+            if (e.type == ExoPlaybackException.TYPE_RENDERER) {
+                Exception cause = e.getRendererException();
+                if (cause instanceof MediaCodecRenderer.DecoderInitializationException) {
+                    // Special case for decoder initialization failures.
+                    MediaCodecRenderer.DecoderInitializationException decoderInitializationException =
+                            (MediaCodecRenderer.DecoderInitializationException) cause;
+                    if (decoderInitializationException.codecInfo == null) {
+                        if (decoderInitializationException.getCause() instanceof MediaCodecUtil.DecoderQueryException) {
+                            errorString = "不能查询到设备的解码器";
+                        } else if (decoderInitializationException.secureDecoderRequired) {
+                            errorString = "设备没有提供安全的解码器" + decoderInitializationException.mimeType;
+                        } else {
+                            errorString =
+                                    "设备没有提供解码器" + decoderInitializationException.mimeType;
+                        }
+                    } else {
+                        errorString = "不能实例化解码器";
+                    }
+                }
+            }
+            return Pair.create(0, errorString);
+        }
+    }
+
+    private class PlayerEventListener implements Player.EventListener {
+
+        @Override
+        public void onPlayerStateChanged(boolean playWhenReady, @Player.State int playbackState) {
+
+
+        }
+
+        @Override
+        public void onPlayerError(ExoPlaybackException e) {
+
+        }
+
+        @Override
+        @SuppressWarnings("ReferenceEquality")
+        public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+        }
+    }
+
+    public DataSource.Factory buildDataSourceFactory() {
+        DefaultDataSourceFactory upstreamFactory =
+                new DefaultDataSourceFactory(this, buildHttpDataSourceFactory());
+        return buildReadOnlyCacheDataSource(upstreamFactory, getDownloadCache());
+    }
+
+    /**
+     * Returns a {@link HttpDataSource.Factory}.
+     */
+    public HttpDataSource.Factory buildHttpDataSourceFactory() {
+        return new DefaultHttpDataSourceFactory(Util.getUserAgent(this, "ExoPlayerDemo"));
+    }
+
+    protected static CacheDataSourceFactory buildReadOnlyCacheDataSource(
+            DataSource.Factory upstreamFactory, Cache cache) {
+        return new CacheDataSourceFactory(
+                cache,
+                upstreamFactory,
+                new FileDataSource.Factory(),
+                /* cacheWriteDataSinkFactory= */ null,
+                CacheDataSource.FLAG_IGNORE_CACHE_ON_ERROR,
+                /* eventListener= */ null);
+    }
+
+    private Cache downloadCache;
+
+    protected synchronized Cache getDownloadCache() {
+        if (downloadCache == null) {
+            File downloadContentDirectory = new File(getDownloadDirectory(), "downloads");
+            downloadCache = new SimpleCache(downloadContentDirectory, new NoOpCacheEvictor(), getDatabaseProvider());
+        }
+        return downloadCache;
+    }
+
+    private File downloadDirectory;
+    private DownloadManager downloadManager;
+
+    private File getDownloadDirectory() {
+        if (downloadDirectory == null) {
+            downloadDirectory = getExternalFilesDir(null);
+            if (downloadDirectory == null) {
+                downloadDirectory = getFilesDir();
+            }
+        }
+        return downloadDirectory;
+    }
+
+    private DatabaseProvider databaseProvider;
+
+    private DatabaseProvider getDatabaseProvider() {
+        if (databaseProvider == null) {
+            databaseProvider = new ExoDatabaseProvider(this);
+        }
+        return databaseProvider;
+    }
+}
